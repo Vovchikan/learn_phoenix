63c81894e88b129c5b5ef0e9c6426cb46f54a052
Vladimir Samorodov <vovchikan@gmail.com>
2022-12-27 07:24:15 +0000

    First LiveView experience

---
+++ lib/rumbl_web/live/clock_live.ex
@@ -0,0 +1,26 @@
+defmodule RumblWeb.ClockLive do
+  use RumblWeb, :live_view
+  import Calendar
+
+  def render(assigns) do
+    ~L"""
+    <div>
+    <h2>Date:  <%= strftime(@date, "%d-%m-%Y" ) %></h2>
+    <h2>Time:  <%= strftime(@time, "%X" ) %></h2>
+    </div>
+    """
+  end
+
+  def mount(_params, _session, socket) do
+    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
+    {:ok, put_datetime(socket)}
+  end
+
+  defp put_datetime(socket) do
+    assign(socket, date: Date.utc_today(), time: Time.utc_now())
+  end
+
+  def handle_info(:tick, socket) do
+    {:noreply, put_datetime(socket)}
+  end
+end
